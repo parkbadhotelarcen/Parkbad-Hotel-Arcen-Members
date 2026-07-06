d742773e5fd5ad49756388effc18124f50e79b82
parkbadhotelarcen <parkbadhotelarcen@gmail.com>
2026-07-06 11:37:09 +0200

    Add app configuration constants

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+export const appUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+export const clubName = "Parkbad Hotel Arcen Members";
+export const hotelName = "Parkbad Hotel Arcen";
+
+export const managerRoles = ["manager", "admin"] as const;
